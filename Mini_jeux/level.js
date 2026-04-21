c9cbf2126eef017aab257a022622f2f0e83d0bc6
Amaryyyy <amary.lebideau@gmail.com>
2026-04-21 09:58:29 +0200

    Update level.js

--- Mini_jeux/level.js
+++ Mini_jeux/level.js
@@ -35,6 +35,7 @@ const levels = [
   { game: 'game33', difficulty: 'hard' },
   { game: 'game34', difficulty: 'hard' },
   { game: 'game35', difficulty: 'hard' },
+  { game: 'game36', difficulty: 'hard' },
 ];
 
 // Get the next level

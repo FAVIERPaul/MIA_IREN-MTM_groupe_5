cded871b061049bd9adb4b0aa73ea41c062d678f
clara <clara.brehon@gmail.com>
2026-04-21 10:12:24 +0200

    update

--- Mini_Jeux/test_debug.js
+++
@@ -1,9 +0,0 @@
-// Test simple pour vérifier que gameManager fonctionne
-import { gameManager } from "./gameCleanup.js";
-
-console.log("gameManager:", gameManager);
-console.log("gameManager.isRunning avant startGame:", gameManager.isRunning);
-gameManager.startGame();
-console.log("gameManager.isRunning après startGame:", gameManager.isRunning);
-gameManager.cleanup();
-console.log("gameManager.isRunning après cleanup:", gameManager.isRunning);
